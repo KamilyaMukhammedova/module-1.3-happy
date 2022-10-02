eafe690369c8582be15eafec08f4c1861c724b78
Kamilya Mukahmmedova <kamilichkam@gmail.com>
2022-10-02 22:18:56 +0500

    Timer-block (adaptive styles)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './index.html';
 import './index.scss';
 import './js/open-close';
+import './js/timer';
 
--- webpack.config.js
+++ webpack.config.js
@@ -67,6 +67,16 @@ module.exports = {
         test: /\.(png|jpg|svg|gif)$/,
         type: 'asset/resource',
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env",]
+          }
+        }
+      }
     ]
   }
 }
